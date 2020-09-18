856f94196671f1162cf993bde24f6ee824d01874
ryuuto829 <ryuuto829@gmail.com>
2020-09-18 18:30:53 +0300

    🚀 Initialize project

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const App = () => (
+  <>
+    <StyledHeader>Hello</StyledHeader>
+  </>
+);
+
+const StyledHeader = styled.h1`
+  color: #ccc;
+  margin: 10px;
+`;
+
+export default App;
