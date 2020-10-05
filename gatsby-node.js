eec1a41cf73fb7f440ba2caa5a22f9bf14a260c8
ryuuto829 <ryuuto829@gmail.com>
2020-10-05 21:29:05 +0300

    chore: fix prettier, add more path shortcuts

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,7 +13,8 @@ exports.onCreateWebpackConfig = ({ actions }) => {
         '@components': path.resolve(__dirname, 'src/components'),
         '@config': path.resolve(__dirname, 'src/config'),
         '@pages': path.resolve(__dirname, 'src/pages'),
-        '@styles': path.resolve(__dirname, 'src/styles')
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@hooks': path.resolve(__dirname, 'src/hooks')
       }
     }
   });
