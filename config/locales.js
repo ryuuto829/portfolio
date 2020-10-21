8744a46edc24b2a8848c99b1dc4bbcae94e77320
ryuuto829 <ryuuto829@gmail.com>
2020-10-21 20:04:16 +0300

    feat: add i18n languages config and locale navLinks config

---
+++ config/locales.js
@@ -0,0 +1,13 @@
+// Only one language should have the "default: true" key
+
+module.exports = {
+  en: {
+    path: 'en',
+    locale: 'en-US',
+    default: true
+  },
+  uk: {
+    path: 'uk',
+    locale: 'uk_UA'
+  }
+};
