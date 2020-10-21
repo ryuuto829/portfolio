b017705e3f57e8ee4f39ed9f7dddbeb2e907e86f
ryuuto829 <ryuuto829@gmail.com>
2020-10-21 20:07:23 +0300

    feat: add custom hook for changing localization
    * Hook provides global context storage for current locale param
    * And also local provider with changing function

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useActiveLinkObserver } from './useActiveLinkObserver';
 export { default as useScrollDirection } from './useScrollDirection';
 export { default as useScrolledToTop } from './useScrolledToTop';
+export { LocaleProvider, useLocale } from './useLocale';
