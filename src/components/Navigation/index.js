d1c76d6f80915709aac962a49be4890955a34809
ryuuto829 <ryuuto829@gmail.com>
2020-10-23 23:07:44 +0300

    feat: add graphql query for navigation links

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { navLinks } from '@config';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import * as S from './styled';
+import { useScrollDirection, useScrolledToTop, useLocale } from '@hooks';
 
 import Logo from '../icons/Logo';
 import NavigationLinks from '../NavigationLinks';
 import LanguagesMenu from '../LanguagesMenu';
 import ButtonTheming from '../ButtonTheming';
-import { useScrollDirection, useScrolledToTop } from '@hooks';
+
+const query = graphql`
+  query useNavLinks {
+    allFile(filter: { relativeDirectory: { eq: "navLinks" } }) {
+      edges {
+        node {
+          name
+          childNavLinksJson {
+            navLinks {
+              name
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Navigation = () => {
   const scrollDirection = useScrollDirection();
   const scrolledToTop = useScrolledToTop();
+  const { locale } = useLocale();
+
+  // Query the JSON files in ./config/i18n/navLinks
+  const { allFile } = useStaticQuery(query);
+
+  // Extract all lists from GraphQL query response
+  const queryList = allFile.edges.map(item => ({
+    name: item.node.name,
+    navLinks: item.node.childNavLinksJson.navLinks
+  }));
+
+  // Return list of links for the current locale
+  const { navLinks } = queryList.filter(lang => lang.name === locale)[0];
 
   return (
     <S.NavWrapper
