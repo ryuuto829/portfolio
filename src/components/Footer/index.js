ff186fb7920797930674e54c5a9c51b99291f04d
ryuuto829 <ryuuto829@gmail.com>
2020-10-30 19:32:19 +0200

    feat: add translations to footer via graphql query

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,37 +1,67 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import * as S from './styled';
+import { useTranslation } from '@hooks';
 
 import { socialLinks } from '@config';
 
-const Footer = () => (
-  <>
-    <S.SocialMenu>
-      {socialLinks &&
-        socialLinks.map(({ url, name }, i) => (
-          <S.SocialItem key={i}>
-            <S.SocialLink
-              href={url}
-              aria-label={name}
-              target="_blank"
-              title={name}
-              rel="noopener noreferrer">
-              {name}
-            </S.SocialLink>
-          </S.SocialItem>
-        ))}
-    </S.SocialMenu>
-    <S.Credit>© 2020 Dmytro Rykhlyk. All Rights Reserved.</S.Credit>
-    <S.Credit>
-      Built with{' '}
-      <S.GatsbyLink
-        href="https://www.gatsbyjs.org/"
-        target="_blank"
-        rel="noopener noreferrer">
-        Gatsby
-      </S.GatsbyLink>{' '}
-      | Hosted on ...
-    </S.Credit>
-  </>
-);
+const query = graphql`
+  query useCreditInfo {
+    allFile(filter: { relativeDirectory: { eq: "translations" } }) {
+      edges {
+        node {
+          childTranslationsJson {
+            creditInfo {
+              name
+              rights
+            }
+          }
+          name
+        }
+      }
+    }
+  }
+`;
+
+const Footer = () => {
+  // Query the JSON files in ./config/i18n/index
+  const { allFile } = useStaticQuery(query);
+  const { creditInfo } = useTranslation(allFile);
+
+  const year = new Date().getFullYear();
+
+  return (
+    <>
+      <S.SocialMenu>
+        {socialLinks &&
+          socialLinks.map(({ url, name }, i) => (
+            <S.SocialItem key={i}>
+              <S.SocialLink
+                href={url}
+                aria-label={name}
+                target="_blank"
+                title={name}
+                rel="noopener noreferrer">
+                {name}
+              </S.SocialLink>
+            </S.SocialItem>
+          ))}
+      </S.SocialMenu>
+      <S.Credit>
+        {`© ${year} ${creditInfo.name}. ${creditInfo.rights}`}
+      </S.Credit>
+      <S.Credit>
+        Built with{' '}
+        <S.GatsbyLink
+          href="https://www.gatsbyjs.org/"
+          target="_blank"
+          rel="noopener noreferrer">
+          Gatsby
+        </S.GatsbyLink>{' '}
+        | Hosted on ...
+      </S.Credit>
+    </>
+  );
+};
 
 export default Footer;
