9eac1305bc3520bb91a087cf439a4aeaa17e89a8
ryuuto829 <ryuuto829@gmail.com>
2020-11-04 18:35:40 +0200

    feat: add icons for project cards

--- src/icons/Icon.js
+++ src/icons/Icon.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import { default as IconGithub } from '@icons/Github';
 import { default as IconLinkedin } from '@icons/Linkedin';
 import { default as IconCodepen } from '@icons/Codepen';
+import { default as IconArrowForward } from '@icons/ArrowForward';
+import { default as IconLink } from '@icons/Link';
+import { default as IconLogo } from '@icons/Logo';
+import { default as IconSun } from '@icons/Sun';
 
 const Icon = ({ name }) => {
   switch (name) {
@@ -12,6 +16,14 @@ const Icon = ({ name }) => {
       return <IconLinkedin />;
     case 'Codepen':
       return <IconCodepen />;
+    case 'ArrowForward':
+      return <IconArrowForward />;
+    case 'Link':
+      return <IconLink />;
+    case 'Logo':
+      return <IconLogo />;
+    case 'Sun':
+      return <IconSun />;
     default:
       return null;
   }
