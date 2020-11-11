7f337f763504823e8d4b5e254fe6537d65c2c211
ryuuto829 <ryuuto829@gmail.com>
2020-11-11 19:03:12 +0200

    feat: add hero section
    * Add titles and small bio
    * Make list of links with custom buttons
    * Add styling to the hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import * as S from './styled';
 
-const Hero = () => (
-  <S.HeroSection id="home">
-    <h1>MY NAME</h1>
-  </S.HeroSection>
-);
+import { useTranslation } from '@hooks';
+
+const Hero = () => {
+  const { title, subTitle, greeting, smallBio } = useTranslation();
+
+  return (
+    <S.HeroSection id="home">
+      <span>{greeting}</span>
+      <h1>{title}</h1>
+      <h2>{subTitle}</h2>
+      <p>{smallBio}</p>
+
+      <S.LinksGroup>
+        <li>
+          <a href="/">
+            <span className="text">Resume</span>
+            <span className="circle" aria-hidden="true">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                enableBackground="new 0 0 24 24"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24">
+                <rect fill="none" height="24" width="24" />
+                <path d="M9,5v2h6.59L4,18.59L5.41,20L17,8.41V15h2V5H9z" />
+              </svg>
+            </span>
+          </a>
+        </li>
+
+        <li>
+          <a href="mailto:rykhlyk.info@gmail.com">
+            rykhlyk.info@gmail.com
+            <span className="circle" aria-hidden="true">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z" />
+              </svg>
+            </span>
+          </a>
+          <button>Copy</button>
+        </li>
+      </S.LinksGroup>
+    </S.HeroSection>
+  );
+};
 
 export default Hero;
--- src/components/Hero/styled.js
+++ src/components/Hero/styled.js
@@ -1,8 +1,120 @@
 import styled from 'styled-components';
 
 export const HeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexColumn};
+
   min-height: 100vh;
   padding: var(--nav-height) 0;
   max-width: 1000px;
   margin: 0px auto;
+
+  h1,
+  h2 {
+    font-size: clamp(40px, 8vw, 80px);
+    /* font-size: clamp(40px, 8vw, 60px); */
+    margin: 0 0 10px 0;
+    font-family: var(--family-secondary);
+    line-height: 1.2;
+    font-weight: var(--weight-bold);
+  }
+
+  & > span {
+    margin-bottom: 30px;
+    font-size: 25px;
+  }
+
+  p {
+    margin: 30px 0 0 0;
+    max-width: 500px;
+    font-size: 20px;
+    color: #8a919e;
+  }
+`;
+
+export const LinksGroup = styled.ul`
+  ${({ theme }) => theme.mixins.list};
+
+  display: flex;
+  position: relative;
+
+  font-size: 16px;
+  margin-top: 50px;
+
+  li {
+    ${({ theme }) => theme.mixins.flexItemsCenter};
+    position: relative;
+    margin-right: 10px;
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 48px;
+      height: 48px;
+      background-color: #27272c;
+      border-radius: 34px;
+      transition: var(--transition);
+      z-index: 1;
+    }
+
+    &:hover {
+      &:before {
+        width: 100%;
+      }
+
+      svg {
+        transform: translateX(15px);
+      }
+    }
+  }
+
+  a {
+    ${({ theme }) => theme.mixins.link};
+    display: block;
+    padding: 10px;
+    position: relative;
+    padding-left: 58px;
+    padding-right: 20px;
+    color: ${({ theme }) => theme.colorMainText};
+    z-index: 100;
+    line-height: 24px;
+  }
+
+  .circle {
+    ${({ theme }) => theme.mixins.flexItemsCenter};
+    justify-content: flex-start;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 48px;
+    height: 48px;
+    background-color: #27272c;
+    border-radius: 34px;
+    transition: var(--transition);
+    z-index: 1;
+
+    svg {
+      transition: var(--transition);
+      transform: translateX(12px);
+      fill: ${({ theme }) => theme.colorMainText};
+    }
+  }
+
+  button {
+    ${({ theme }) => theme.mixins.button};
+
+    padding: 6px;
+    background-color: #dfdfdf;
+    color: var(--almost-black);
+    border-radius: 4px;
+    z-index: 100;
+    position: relative;
+    margin-right: 20px;
+    transition: var(--transition);
+
+    &:hover {
+      background-color: var(--light-blue);
+    }
+  }
 `;
