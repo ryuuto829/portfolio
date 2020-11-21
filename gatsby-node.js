6b570095e83c5520214cf4051f7eef3347b42416
ryuuto829 <ryuuto829@gmail.com>
2020-11-21 16:29:27 +0200

    feat: create page for each project from markdown
    * Using gatsby-node functions, add localized slug and locale to all markdown files
    * Using project template, create pages with context from markdown files
    * Add helper function to find default language from the locales config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const locales = require('./config/i18n/locales');
+const { findKey } = require('./src/utils/findKey');
 
 // Add Absolute imports definitions
 // https://www.gatsbyjs.com/docs/add-custom-webpack-config/#absolute-imports
 
 exports.onCreateWebpackConfig = ({ actions }) => {
+  // We can use shorcuts like '@components/File' insead of '../../File'
   actions.setWebpackConfig({
     resolve: {
       alias: {
@@ -26,6 +28,8 @@ exports.onCreateWebpackConfig = ({ actions }) => {
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage, deletePage } = actions;
 
+  // For each page, we’re deleting it, than creating it again for each
+  // language passing the locale to the page context.
   return new Promise(resolve => {
     deletePage(page);
 
@@ -44,5 +48,107 @@ exports.onCreatePage = ({ page, actions }) => {
     });
 
     resolve();
+  });
+};
+
+// Use i18n for markdown
+// https://github.com/gatsbyjs/gatsby/blob/cba8b707515b2169738f2b47af4559e3a158b68c/examples/using-i18n/gatsby-node.js#L9-L38
+
+// Correcting language and slug to the frontmatter of each file
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions;
+
+  // Check for all markdown files
+  if (node.internal.type === `MarkdownRemark`) {
+    // https://nodejs.org/api/path.html#path_path_basename_path_ext
+    // It will return the file name without '.md' string (e.g. "file-name" or "file-name.lang")
+    const name = path.basename(node.fileAbsolutePath, `.md`);
+
+    // Find the key that has "default: true" set
+    const defaultKey = findKey(locales, o => o.default === true);
+
+    // Check if file.name.lang has the default lang type
+    const isDefault = name.split(`.`)[1] === defaultKey;
+
+    // Files are defined with "name-with-dashes.lang.md"
+    // If it's the default language, pass the locale for that
+    const lang = isDefault ? defaultKey : name.split(`.`)[1];
+
+    // Create Localized Slug like "/projects/name" or "uk/projects/name"
+    const slug = isDefault
+      ? `/projects/${name.split(`.`)[0]}`
+      : `/${lang}/projects/${name.split(`.`)[0]}`;
+
+    // Adding the nodes on GraphQL for each post as "fields"
+    createNodeField({ node, name: `slug`, value: slug });
+    createNodeField({ node, name: `locale`, value: lang });
+    createNodeField({ node, name: `isDefault`, value: isDefault });
+  }
+};
+
+// Create page for all projects markdown files
+// https://www.gatsbyjs.com/docs/adding-markdown-pages/
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions;
+
+  const projectTemplate = require.resolve(`./src/templates/project.js`);
+
+  const projectList = await graphql(`
+    query {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/content/projects/" } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+              about
+              external
+              github
+              technologies
+            }
+            fields {
+              isDefault
+              locale
+              slug
+            }
+            html
+          }
+        }
+      }
+    }
+  `);
+
+  // Handle errors
+  if (projectList.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  const projectContent = projectList.data.allMarkdownRemark.edges;
+
+  // Creating project page for each markdown file
+  projectContent.forEach(({ node }) => {
+    const { slug, locale, isDefault } = node.fields;
+    const { title, date, external, github, technologies } = node.frontmatter;
+
+    createPage({
+      path: slug,
+      component: projectTemplate,
+      context: {
+        // additional data can be passed via context
+        slug,
+        locale,
+        isDefault,
+        title,
+        date,
+        external,
+        github,
+        technologies,
+        html: node.html
+      }
+    });
   });
 };
