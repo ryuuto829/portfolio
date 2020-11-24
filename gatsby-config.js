b92db5326245228ac57b834eacef260c80928a78
ryuuto829 <ryuuto829@gmail.com>
2020-11-24 19:10:32 +0200

    build: add deps for animations and transitions

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
